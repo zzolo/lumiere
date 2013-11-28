b842ba13b70e8819b9edfd7c64e96bbaa7fff64c
Alan Palazzolo <apalazzolo@minnpost.com>
2013-11-28 09:01:12 -0600

    Adding in bower components and basic ractive handling.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,36 @@
 /**
  * Main client side logic.
  */
-(function() {
-  var socket = io.connect();
+require.config({
+  shim: {
+    'underscore': {
+      exports: '_'
+    }
+  },
+  paths: {
+    requirejs: '../bower_components/requirejs/require',
+    text: '../bower_components/text/text',
+    jquery: '../bower_components/jquery/jquery.min',
+    underscore: '../bower_components/underscore/underscore-min',
+    io: '/socket.io/socket.io',
+    Ractive: '../bower_components/ractive/build/Ractive.min'
+  }
+});
 
-  socket.on('status', function (data) {
-    console.log(data);
+require(['jquery', 'underscore', 'io', 'Ractive', 'text!../templates/app.ractive'], function($, _, io, Ractive, appTemplate) {
+  var socket = io.connect();
+  var ractive;
+  var tData = { status: 'disconnected' };
+
+  // Create ractive handler
+  ractive = new Ractive({
+    el: '#template-container',
+    template: appTemplate,
+    data: tData
   });
 
-})();
+  // Update status
+  socket.on('status', function(data) {
+    ractive.set('status', data);
+  });
+});
